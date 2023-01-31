caff05c0fc80e5b48203852dfb9331894f87c891
Rongjian Zhang <pd4d10@gmail.com>
2023-01-31 11:02:47 +0800

    chore: update svelte build

--- packages/svelte/build.js
+++ packages/svelte/build.js
@@ -1,8 +1,9 @@
 // @ts-check
-import { sveltePreprocessor } from '../../scripts/utils.mjs'
+/// <reference lib="esnext" />
 import fs from 'fs-extra'
 import path from 'path'
 import { emitDts } from 'svelte2tsx'
+import { sveltePreprocess } from 'svelte-preprocess/dist/autoProcess.js'
 import { preprocess } from 'svelte/compiler'
 
 fs.ensureDirSync('dist')
@@ -11,15 +12,19 @@ fs.emptyDirSync('dist')
 // some parts are from here https://github.com/sveltejs/kit/blob/master/packages/kit/src/packaging/typescript.js
 await emitDts({
   svelteShimsPath: 'node_modules/svelte2tsx/svelte-shims.d.ts',
-  declarationDir: '.',
+  declarationDir: 'dist',
 })
 
 const files = fs.readdirSync('src')
 for (let file of files) {
   const source = fs.readFileSync(path.resolve('src', file), 'utf8')
-  const { code } = await preprocess(source, sveltePreprocessor, {
-    filename: file,
-  })
+  const { code } = await preprocess(
+    source,
+    sveltePreprocess({
+      typescript: { compilerOptions: { rootDir: '.' } },
+    }),
+    { filename: file }
+  )
   fs.writeFileSync(
     path.resolve('dist', file),
     code.replaceAll(' lang="ts"', '')
